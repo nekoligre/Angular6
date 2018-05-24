ff9e4a0a82b5702f83a027cc85932e1e41a92567
alberto gonzalez <alberto.gonzalez@gft.com>
2018-05-24 20:23:36 +0200

    feat(): prueba añadiendo d3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, AfterContentInit } from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements AfterContentInit {
+	title = 'app';
+
+	radius = 10;
+
+	ngAfterContentInit() {
+		const values = [1, 2, 3, 4, 5];
+
+		d3.select('body')
+			.selectAll('p')
+			.data(values)
+			.enter()
+			.append('p')
+			.text((e) => e);
+
+	}
+
+	clicked(event: any) {
+		// d3.select(event.target).append('circle')
+		// 	.attr('cx', event.x + 100)
+		// 	.attr('cy', event.y + 50)
+		// 	.attr('r', () => {
+		// 		return this.radius;
+		// 	})
+		// 	.attr('fill', 'red');
+	}
+
 }
